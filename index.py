d93f64a01c1107739f77ddbbe9a11dbfee87548f
Devansh <111366687+Devansh682@users.noreply.github.com>
2023-03-05 10:24:28 +0530

    minor update

--- index.py
+++ index.py
@@ -7,8 +7,9 @@ percent_attended = ((total_attended_hours / total_hours) * 100)
 
 percent_attended = round(percent_attended, 2)
 
-print("Percent Attented : " + str(percent_attended))
+print("Percent Absent : " + str(percent_attended))
 
 ans = round((100 - percent_attended), 2)
 
-print("Percent Absent : " + str(ans))
+print("Percent Attended : " + str(ans))
+#adios
